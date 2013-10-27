29ce2488794d300588e903e6797bb61677b16979
Mansfield Mark <mmark@andrew.cmu.edu>
2013-10-27 18:04:35 -0400

    Working on interfacing sk with html

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 var spawn   = require('child_process').spawn;
+var exec    = require('child_process').exec;
 var express = require('express');
 var app     = express();
 
 app.use(express.static(__dirname));
+
+app.get('/testSk', function(req, res) {
+  console.log("Testing sk");
+  //var command = spawn('../sk/sleuthkit-4.1.2/tools/fstools/fsstat', ['../images/ntfs-img-kw-1.dd'])
+  console.log("dirname: "+__dirname);
+  var command = exec(__dirname+'/test.sh',
+    function(error,stdout,stderr) {
+      console.log('error: '+error);
+      console.log('stdout: '+stdout);
+    });
+});
 
 app.get('/colorsRequest', function(req, res) {
 //  var command = spawn(__dirname + '/run.sh', [ req.query.color || '' ]);
