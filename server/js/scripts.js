d214334a124aeaaeb7bfd72b444553df9a0ae5b8
Eugene Choi <eugene@eugene-laptop1.(none)>
2013-12-02 19:42:42 -0500

    asdf

--- server/js/scripts.js
+++ server/js/scripts.js
@@ -1,81 +1,69 @@
 function exec_cmd()
 {
   $.get('/exec_cmd', { cmd : $('#command').val(),
-    imagePath : $('#imagePath').val(),
-  toolPath : $('#toolPath').val()})
+      imagePath : $('#imagePath').val(),
+      toolPath : $('#toolPath').val()})
     .done(function(data) {
-      $('#output').val(data);
+        $('#output').val(data);
     });
 }
 
 function get_images()
 {
   $.get('/get_images', null)
-    .done(function(res)
+    .done(function(res) {
+        $('#output').val(res.output);
+        var all_imgs = res.imgs;
+        $('#img_list').empty();
+        for(var i = 0; i < all_imgs.length; i++)
         {
-          $('#output').val(res.output);
-          var all_imgs = res.imgs;
-          $('#img_list').empty();
-          for(var i = 0; i < all_imgs.length; i++)
-    {
-      var opt = document.createElement("option");
-      opt.text = all_imgs[i];
-      opt.value = all_imgs[i];
-      $('#img_list').append(opt);
-    }
-        });
+          var opt = document.createElement("option");
+          opt.text = all_imgs[i];
+          opt.value = all_imgs[i];
+          $('#img_list').append(opt);
+        }
+    });
 }
 
 function select_image()
 {
   $.get('/select_image', { img_name : $('#img_list').val() })
-    .done(function(res)
-        {
-          if(res.success)
-      alert("Yay success!");
-          else
-      alert("Boo failure!");
-        });
+    .done(function(res) {
+        if(res.success) alert("Yay success!");
+        else alert("Boo failure!");
+    });
 }
 
 function load_db()
 {
   $.get('/load_db', null)
-    .done(function(res)
-        {
-          if(res.success)
-    {
-      alert("Yay success!");
-    }
-          else
-      alert("Boo failure!");
-        });
+    .done(function(res) {
+        if(res.success) alert("Yay success!");
+        else alert("Boo failure!");
+    });
 }
 
 function list_files()
 {
   $.get('/list_files', null)
-    .done(function(res)
-        {
-          if(res.success)
-    {
-      alert("Yay success!");
-      $('#output').val(res.file_list);
-    }
-          else
-      alert("Boo failure!");
-        });
+    .done(function(res) {
+        if(res.success) {
+          alert("Yay success!");
+          $('#output').val(res.file_list);
+        }
+        else alert("Boo failure!");
+    });
 }
 
-$(function(){
-
+$(function() {
   $('#submit').click(exec_cmd);
 
   $('#clear').click(function() {
     $('#output').val('');
   });
 
-  $('#command').keyup(function(event) {
+  $('#command').keyup (function(event) {
+    // Hit the Enter key
     if (event.keyCode == 13) {
       $('#submit').click();
     }
@@ -84,6 +72,4 @@ $(function(){
   $('#get_imgs').click(get_images);
 
   $('#select_img').click(select_image);
-
-
 });
